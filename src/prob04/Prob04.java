8facaf458e6cc4669e404b369e57ddd2a9383faa
BIT <BIT@DESKTOP-NMF9F4U>
2019-08-07 17:06:37 +0900

    4번문제 완료

--- src/prob04/Prob04.java
+++ src/prob04/Prob04.java
@@ -10,11 +10,20 @@ public class Prob04 {
 	}
 	
 	public static char[] reverse(String str) {
+		char[] copystr = str.toCharArray();
+		char[] reversestr = new char[str.length()];
+		int j = str.length() - 1;
+		for(int i=0; i<str.length(); i++) {
+			reversestr[i] = copystr[j];
+			j--;
+		}
 		/* 코드를 완성합니다 */
-		return null;
+		return reversestr;
 	}
 
 	public static void printCharArray(char[] array){
 		/* 코드를 완성합니다 */
+		for(int i=0; i<array.length; i++) 
+			System.out.print( array[i] );
 	}
 }
