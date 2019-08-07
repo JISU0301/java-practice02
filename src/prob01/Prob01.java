9824fdd59621637199685c5c698d346ea9e561da
BIT <BIT@DESKTOP-NMF9F4U>
2019-08-07 15:51:24 +0900

    1번문제 완료

--- src/prob01/Prob01.java
+++ src/prob01/Prob01.java
@@ -4,11 +4,25 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
-		
+		int i=50000, sw=0, money, count;
+		Scanner sc = new Scanner( System.in  );
 		System.out.print( "금액: " );
-		int money = scanner.nextInt();
+		money = sc.nextInt();
 		
-		scanner.close();
+		while(true) {
+			count = money/i;
+			System.out.printf("%d원 : %d매\n",i,count);
+			if(i<=1)
+				break;
+			money = money - (count*i);
+			
+			if(sw==0) {
+				i=i/5;
+				sw=1;
+			}else {
+				i=i/2;
+				sw=0;
+			}
+		}
  	}
 }
