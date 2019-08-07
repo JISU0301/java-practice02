934e7633d13dd562a3a1515115f134905d08c314
BIT <BIT@DESKTOP-NMF9F4U>
2019-08-07 16:35:44 +0900

    3번문제 완료

--- src/prob03/Prob03.java
+++ src/prob03/Prob03.java
@@ -4,19 +4,25 @@ public class Prob03 {
 	public static void main(String args[]) {
 		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
-		// 원래 배열 원소 출력
-		printCharArray(c);
+		
+		printCharArray(c);// 원래 배열 원소 출력
 
-		// 공백 문자 바꾸기
-		replaceSpace(c);
-
-		// 수정된 배열 원소 출력
-		printCharArray(c);
+		
+		replaceSpace(c);// 공백 문자 바꾸기
+		 System.out.println();
+		
+		printCharArray(c);// 수정된 배열 원소 출력
 	}
 
 	public static void replaceSpace(char c[]) {
-	}
+		for(int i=0; i<c.length-1; i++) {
+			if(c[i]==' ')
+				c[i]=',';
+		}
+			}
 
 	public static void printCharArray(char c[]) {
+		for(int i=0; i<c.length-1; i++)
+			System.out.print(c[i]);
 	}
 }
